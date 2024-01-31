da5e830495867a30fb606fc7fe6a6d6bef9c243b
sameoldcarlos <albertocarlos221@gmail.com>
2024-01-31 00:43:02 -0300

    fix: optimize prepareUsers function

--- js/user/prepareUsers.js
+++ js/user/prepareUsers.js
@@ -1,24 +1,13 @@
 import newUser from "../user/newUser.js"
-let usersList = [];
 
-const prepareUsers = async (numUsers) => {
-    const urlGetRawUsers = `https://randomuser.me/api/?page=1&results=${numUsers}&format=json&nat=br&inc=gender,email`;
-    const requestRawUsers = async () => {
-        const response = await fetch(urlGetRawUsers).then( res => res.json() )
-        .then( values => fillUsersList(values.results) );
-        return response;        
-    }
+const dividers = ["", "_", "."];
 
-    await requestRawUsers();
-    return usersList;
-}
+async function prepareUsers(numUsers) {
+    const getUsersUrl = `https://randomuser.me/api/?page=1&results=${numUsers}&format=json&nat=br&inc=gender,name,picture`;
+    const {results} = await fetch(getUsersUrl).then(res => res.json());
 
-const fillUsersList = (arrayRawUsers) => {
-    const dividers = ["", "_", "."];
-    arrayRawUsers.forEach(rawUser => {
-        const user = newUser(rawUser, dividers);        
-        usersList.push(user);
-    });
+    return results.map(rawUser => newUser(rawUser, dividers))
 }
 
  export default prepareUsers;
+ 
