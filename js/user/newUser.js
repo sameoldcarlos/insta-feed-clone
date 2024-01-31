c2bc9968272067d376a0ed71d23bfe5c8a72e295
sameoldcarlos <albertocarlos221@gmail.com>
2024-01-31 00:54:28 -0300

    fix: do not allow periods as the username first char

--- js/user/newUser.js
+++ js/user/newUser.js
@@ -3,7 +3,7 @@ import getRandomIntInclusive from "../utils/getRandomIntInclusive.js";
 const newUser = (rawUser, dividers) => {
     const sufixes = ["", `${getRandomIntInclusive(0, 9)}${getRandomIntInclusive(0, 9)}`];
 
-    const middle = dividers[getRandomIntInclusive(0, dividers.length - 1)];
+    const middle = dividers[getRandomIntInclusive(0, dividers.length - 2)];
     const prefix = dividers[getRandomIntInclusive(0, dividers.length - 1)];
     const sufix = sufixes[getRandomIntInclusive(0, sufixes.length - 1)];
     
